d8ca44004f681657d75cdc6657a359a64766ea70
JMARCHESENYC <justinmarchese11@gmail.com>
2015-12-09 15:22:13 -0500

    Almost there

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostsController < ApplicationController  
+class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -9,13 +9,15 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = Post.create
   end
 
   def create
     if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
   end
@@ -25,14 +27,17 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
+      flash[:success] = "Post updated."
       redirect_to posts_path
     else
+      flash[:alert] = "Update failed.  Please check the form."
       render :edit
     end
   end
 
   def destroy
     @post.destroy
+    flash[:success] = "Your post has been deleted."
     redirect_to root_path
   end
 
@@ -45,4 +50,4 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
-end  
+end
